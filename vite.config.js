e72c93a4f40466e5e0f3330c165d3180cde650c6
Dallin <dallinpoulson@gmail.com>
2025-02-24 16:47:09 -0800

    config again

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,11 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		preact({
-			prerender: {
-				enabled: true,
-				renderTarget: '#app',
-				additionalPrerenderRoutes: ['/404'],
-				previewMiddlewareEnabled: true,
-				previewMiddlewareFallback: '/404',
-			},
-		}),
-	],
-	build: {
-		outDir: "dist", // Default output directory for Netlify
-		sourcemap: true, // Useful for debugging
-	  },
+    plugins: [
+        preact() // Removed the prerender config
+    ],
+    build: {
+        outDir: "dist", // Default output directory for Netlify
+        sourcemap: true, // Useful for debugging
+    },
 });
